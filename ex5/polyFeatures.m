958299deba892f3659c6c694e7410062bc17c9af
unknown <t-vilope@MININT-8HD1NLE.europe.corp.microsoft.com>
2015-06-20 03:45:30 +0200

    ex done

--- ex5/polyFeatures.m
+++ ex5/polyFeatures.m
@@ -7,7 +7,8 @@ function [X_poly] = polyFeatures(X, p)
 
 
 % You need to return the following variables correctly.
-X_poly = zeros(numel(X), p);
+% X_poly = zeros(numel(X), p);
+X_poly = [];
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Given a vector X, return a matrix X_poly where the p-th 
@@ -15,9 +16,9 @@ X_poly = zeros(numel(X), p);
 %
 % 
 
-
-
-
+for i=1:p
+  X_poly = [X_poly X.^i];
+end
 
 
 % =========================================================================
